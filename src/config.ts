1401969131ec5b5f5193992dd1764cb92ddee783
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-10-06 08:33:08 +0900

    リンク先の修正

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const LINKS = [
   { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-  { href: "/#download", label: "Download" },
+  { href: "/#about", label: "About" },
+  { href: "/download", label: "Download" },
   { href: "/license", label: "License" },
   { href: "/contact", label: "Contact" },
 ];
