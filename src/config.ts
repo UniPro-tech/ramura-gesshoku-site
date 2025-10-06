6f73fbd8dc98221ed694ae03913e8985df75d05e
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-10-06 14:01:23 +0900

    リンクを修正

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export const LINKS = [
   { href: "/#about", label: "About" },
   { href: "/download", label: "Download" },
   { href: "https://ramura.uniproject.jp/license", label: "License" },
-  { href: "https://ramura.uniproject.jp/contact", label: "Contact" },
+  { href: "https://ramura.uniproject.jp/contacts", label: "Contact" },
 ];
